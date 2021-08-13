389b36e9ca7a91e23209c4272eecee14b36c5716
kanchan-solanke <kanchanssolanke@gmail.com>
2021-08-13 07:25:03 +0530

    to find cheapest hotel in given date range

--- HotelReservation.java
+++ HotelReservation.java
@@ -1,125 +1,149 @@
-import java.util.HashMap;
+
+
 import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.stream.Collectors;
 
-public class Hotel {
-    private HashMap<String, List<Integer>> lakeWood = new HashMap<>();
-    private HashMap<String, List<Integer>> bridgeWood = new HashMap<>();
-    private HashMap<String, List<Integer>> ridgeWood = new HashMap<>();
-    private static int lakeWoodRating;
-    private static int bridgeWoodRating;
-    private static int rigdeWoodRating;
+class Hotel {
+    int id;
+    String name;
+    int rating;
+    int weekdayPrice;
+    int weekendPrice;
 
-    public Hotel() {
-        setLakeWood();
-        setBridgeWood();
-        setRidgeWood();
+    public Hotel(int id, String name, int rating, int weekdayPrice, int weekendPrice) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+        this.weekdayPrice = weekdayPrice;
+        this.weekendPrice = weekendPrice;
     }
 
-    private void setLakeWood() {
-        List<Integer> lakeWoodRewards = new ArrayList<>();
-        lakeWoodRewards.add(80);
-        lakeWoodRewards.add(80);
-
-        List<Integer> lakeWoodRegular = new ArrayList<>();
-        lakeWoodRegular.add(110);
-        lakeWoodRegular.add(90);
-
-        lakeWood.put("Rewards", lakeWoodRewards);
-        lakeWood.put("Regular", lakeWoodRegular);
-        lakeWoodRating = 3;
+    public Integer getWeekdayPrice() {
+        return weekdayPrice;
     }
 
-    private void setBridgeWood() {
-        List<Integer> bridgeWoodRewards = new ArrayList<>();
-        bridgeWoodRewards.add(110);
-        bridgeWoodRewards.add(50);
-
-        List<Integer> bridgeWoodRegular = new ArrayList<>();
-        bridgeWoodRegular.add(160);
-        bridgeWoodRegular.add(60);
-
-        bridgeWood.put("Rewards", bridgeWoodRewards);
-        bridgeWood.put("Regular", bridgeWoodRegular);
-        bridgeWoodRating = 4;
+    public void createHotel() {
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("Enter HotelName: ");
+        name = scan2.nextLine();
+        System.out.println("Enter HotelRating: ");
+        rating = scan2.nextInt();
+        System.out.println("Enter weekdayPrice: ");
+        weekdayPrice = scan2.nextInt();
+        System.out.println("Enter weekendPrice: ");
+        weekendPrice = scan2.nextInt();
     }
-
-    private void setRidgeWood() {
-        List<Integer> ridgeWoodRewards = new ArrayList<>();
-        ridgeWoodRewards.add(100);
-        ridgeWoodRewards.add(40);
-
-        List<Integer> ridgeWoodRegular = new ArrayList<>();
-        ridgeWoodRegular.add(220);
-        ridgeWoodRegular.add(150);
-
-        ridgeWood.put("Rewards", ridgeWoodRewards);
-        ridgeWood.put("Regular", ridgeWoodRegular);
-        rigdeWoodRating = 5;
-    }
-
-    public String minCostHotel(String input) {
-        String[] arr = input.split(",");
-        String customerType = "";
-        if (arr.length > 0) {
-            String tempArr[] = arr[0].split(":");
-            customerType = tempArr[0];
-            arr[0] = tempArr[1];
-        }
-        String hotel = minCostHotel(customerType, arr);
-        return hotel;
-    }
-
-    private String minCostHotel(String customerType, String[] arr) {
-        int numOfWeekdays = 0;
-        int numOfWeekends = 0;
-        int lakeWoodCost = 0;
-        int bridgeWoodCost = 0;
-        int ridgeWoodCost = 0;
-        for (int i = 0; i < arr.length; i++) {
-            String day = arr[i];
-            System.out.println(day);
-            if (day.contains("mon") || day.contains("tue") || day.contains("wed") || day.contains("thu")
-                    || day.contains("fri")) {
-                numOfWeekdays++;
-            } else {
-                numOfWeekends++;
-            }
-        }
-        List<Integer> valuesForLakewood = lakeWood.get(customerType); // 0 - weekday, 1- weekend
-        List<Integer> valuesForBridgeWood = bridgeWood.get(customerType);
-        List<Integer> valuesForRidgeWood = ridgeWood.get(customerType);
-        lakeWoodCost = numOfWeekdays * valuesForLakewood.get(0) + numOfWeekends * valuesForLakewood.get(1);
-        bridgeWoodCost = numOfWeekdays * valuesForBridgeWood.get(0) + numOfWeekends * valuesForBridgeWood.get(1);
-        ridgeWoodCost = numOfWeekdays * valuesForRidgeWood.get(0) + numOfWeekends * valuesForRidgeWood.get(1);
-        String hotel = minCost(lakeWoodCost, bridgeWoodCost, ridgeWoodCost);
-        System.out.println(lakeWoodCost + " " + bridgeWoodCost + " " + ridgeWoodCost);
-        return hotel;
-    }
-
-    private String minCost(int lakeWoodCost, int bridgeWoodCost, int ridgeWoodCost) {
-        int minCost = Math.min(lakeWoodCost, Math.min(bridgeWoodCost, ridgeWoodCost));
-        if (minCost == lakeWoodCost && minCost == bridgeWoodCost) {
-            return bridgeWoodRating > lakeWoodRating ? "BridgeWood" : "LakeWood";
-        } else if (minCost == bridgeWoodCost && minCost == ridgeWoodCost) {
-            return bridgeWoodRating > ridgeWoodCost ? "BridgeWood" : "RidgeWood";
-        } else if (minCost == lakeWoodCost && minCost == ridgeWoodCost) {
-            return lakeWoodCost > ridgeWoodCost ? "LakeWood" : "RidgeWood";
-        } else {
-            if (minCost == lakeWoodCost) {
-                return "LakeWood";
-            } else if (minCost == bridgeWoodCost) {
-                return "BridgeWood";
-            } else {
-                return "RidgeWood";
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        String input = "Rewards: 10Sept2020, 11Sept2020";
-        Hotel hotelRoom = new Hotel();
-        String hotel = hotelRoom.minCostHotel(input);
-        System.out.println(hotel);
-    }
-
 }
+
+class Main {
+    public static void main(String[] args) throws Exception {
+
+        int id;
+        String name;
+        int rating;
+        int weekdayPrice;
+        int weekendPrice;
+        int hotelCount = 3;
+        String userDate;
+        List<Hotel> hotelsList = new ArrayList<Hotel>();
+        Scanner scan2 = new Scanner(System.in);
+
+        hotelsList.add(new Hotel(1, "Lakewood", 3, 110, 90));
+        hotelsList.add(new Hotel(2, "Bridgewood", 3, 900, 90));
+
+        hotelsList.add(new Hotel(3, "ridgewood", 3, 300, 90));
+
+
+        System.out.println("\n----Menu---" +
+                "\n1: AddHotel" +
+                "\n2: ShowHotels" +
+                "\n3: AddRatings" +
+                "\n4: ShowCheapestHotel" +
+                "\nPress 0 to QUIT!..");
+        System.out.println("Enter your choice => ");
+        int choice = scan2.nextInt();
+
+        do {
+            switch (choice) {
+                case 1:
+                    //----- UC1
+                    hotelCount++;
+                    System.out.println("Enter HotelName: ");
+                    name = scan2.next();
+                    System.out.println("Enter HotelRating: ");
+                    rating = scan2.nextInt();
+                    System.out.println("Enter weekdayPrice: ");
+                    weekdayPrice = scan2.nextInt();
+                    System.out.println("Enter weekendPrice: ");
+                    weekendPrice = scan2.nextInt();
+                    // productsList.add(new Product(1,"HP Laptop",25000f));    
+
+                    hotelsList.add(new Hotel(hotelCount, name, rating, weekdayPrice, weekendPrice));
+                    System.out.println("Hotel added Successfully...\n");
+
+                    System.out.println("\n----Menu---" +
+                            "\n1: AddHotel" +
+                            "\n2: ShowHotels" +
+                            "\n3: AddRatings" +
+                            "\n4: ShowCheapestHotel" +
+                            "\nPress 0 to QUIT!..");
+                    System.out.println("Enter your choice => ");
+                    choice = scan2.nextInt();
+
+                    break;
+
+                case 2:
+                    System.out.println("----------- Hotels List ----------\n");
+                    System.out.println("Id \t" + "Name\t" + "Rating\t" + "weekdayPrice\t" + "weekendPrice\t");
+                    hotelsList.stream()
+                            .forEach(hotel -> System.out.println(hotel.id + "\t" +
+                                    hotel.name + "\t" +
+                                    hotel.rating + "\t" +
+                                    hotel.weekdayPrice + "\t\t" +
+                                    hotel.weekendPrice));
+                    System.out.println("----------------------------------------------------------\n");
+
+                    System.out.println("\n----Menu---" +
+                            "\n1: AddHotel" +
+                            "\n2: ShowHotels" +
+                            "\n3: AddRatings" +
+                            "\n4: ShowCheapestHotel" +
+                            "\nPress 0 to QUIT!..");
+                    System.out.println("Enter your choice => ");
+                    choice = scan2.nextInt();
+
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    //----UC 4
+                    System.out.println("----------- Cheapest Hotel ----------\n");
+                    System.out.println("Id \t" + "Name\t" + "Rating\t" + "weekdayPrice\t" + "weekendPrice\t");
+                    Hotel hotel1 = hotelsList.stream()
+                            .min(Comparator.comparingInt(Hotel::getWeekdayPrice)).get();
+
+                    System.out.println(hotel1.id + "\t" +
+                            hotel1.name + "\t" +
+                            hotel1.rating + "\t" +
+                            hotel1.weekdayPrice + "\t\t" +
+                            hotel1.weekendPrice);
+
+                    System.out.println("\n----Menu---" +
+                            "\n1: AddHotel" +
+                            "\n2: ShowHotels" +
+                            "\n3: AddRatings" +
+                            "\n4: ShowCheapestHotel" +
+                            "\nPress 0 to QUIT!..");
+                    System.out.println("Enter your choice => ");
+
+                    choice = scan2.nextInt();
+                    break;
+            }
+        } while (choice != 0);
+    }
+}  
